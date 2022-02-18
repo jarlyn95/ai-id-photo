c1ba7c94e3b734cbd21731fcd2bff4e25d602925
actboy <actboy@actboys-MacBook-Pro.local>
2022-02-18 17:43:25 +0800

    run config

--- src/algo/run.sh
+++ src/algo/run.sh
@@ -3,7 +3,7 @@
 cpu_core_num=($(python -c 'import multiprocessing; print(multiprocessing.cpu_count())' ))
 #pro_num=$(expr $cpu_core_num \* 2 + 1)
 #pro_num=$cpu_core_num
-pro_num=4
+pro_num=1
 export SMART_ID_PHOTO_ENV=prod
 gunicorn api:app -w $pro_num -b :5000 -t 300  \
          --access-logfile ./access.log -D     \
